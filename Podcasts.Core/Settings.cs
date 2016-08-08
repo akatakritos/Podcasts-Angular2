d5a15e44d2f4463d6e2643f0d6f0c89fdf46e2c6
Matt Burke <git@mattburkedev.com>
2016-08-08 09:02:14 -0500

    cleanup

--- Podcasts.Core/Settings.cs
+++ Podcasts.Core/Settings.cs
@@ -1,13 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Podcasts.Core
 {
     public static class Settings
     {
-        public static bool IsDevelopment => true;
+        public static bool IsDevelopment => Environment == RuntimeEnvironment.Development;
+
+        public static bool IsProduction => Environment == RuntimeEnvironment.Production;
+
+        private static readonly Lazy<RuntimeEnvironment> _environment = new Lazy<RuntimeEnvironment>(() => ParseEnum<RuntimeEnvironment>(ConfigurationManager.AppSettings["RuntimeEnvironment"]));
+        public static RuntimeEnvironment Environment => _environment.Value;
+
+        public static bool UseWebpackDevServer => IsDevelopment && "true".Equals(ConfigurationManager.AppSettings["UseWebpackDevServer"], StringComparison.InvariantCultureIgnoreCase);
+
+        public static string WebpackDevServerUrl => ConfigurationManager.AppSettings["WebpackDevServerUrl"];
+
+        private static T ParseEnum<T>(string s)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+                return default(T);
+
+            return (T)Enum.Parse(typeof(T), s, ignoreCase: true);
+        }
+    }
+
+    public enum RuntimeEnvironment
+    {
+        Development = 0,
+        Production = 1
     }
 }
