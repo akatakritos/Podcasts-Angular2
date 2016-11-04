4b164fe45659be0d83008f097398538b1d6d5d5b
Matt Burke <git@mattburkedev.com>
2016-11-04 08:27:24 -0500

    remove react statics and replace with angular 2

--- statics/src/reducer.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux';
-import podcasts from './podcasts/podcasts_reducer';
-import { reducer as formReducer } from 'redux-form';
-
-export default combineReducers({
-    podcasts,
-    form: formReducer
-});
