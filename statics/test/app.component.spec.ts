e80ae1226750dd568211f0e5830382b5330411d2
Matt Burke <git@mattburkedev.com>
2016-12-12 10:14:58 -0600

    Fix missing expression in test

--- statics/test/app.component.spec.ts
+++ statics/test/app.component.spec.ts
@@ -6,6 +6,6 @@ describe('App', () => {
   });
   it ('should work', () => {
     let fixture = TestBed.createComponent(AppComponent);
-    expect(fixture.componentInstance instanceof AppComponent).toBe(, 'should create AppComponent');
+    expect(fixture.componentInstance instanceof AppComponent).toBe(true, 'should create AppComponent');
   });
 });
