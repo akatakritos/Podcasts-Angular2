32ced52870c6fc1976e36d2723519bb9aec699c8
Matt Burke <git@mattburkedev.com>
2016-12-22 23:07:37 -0600

    Delete all the generic crap that comes in the default project

--- Podcasts.Web/Global.asax.cs
+++ Podcasts.Web/Global.asax.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 using System.Web.Mvc;
-using System.Web.Optimization;
 using System.Web.Routing;
 
 using Podcasts.Web.App_Start;
@@ -19,7 +18,6 @@ namespace Podcasts.Web
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
--- Podcasts.Web/Startup.cs
+++ Podcasts.Web/Startup.cs
@@ -8,7 +8,6 @@ namespace Podcasts.Web
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
         }
     }
 }
